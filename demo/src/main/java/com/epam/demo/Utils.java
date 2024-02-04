fe03c475a3e98178500113c6ec07184eab45f679
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-04 20:10:27 +0300

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+
+        for(String str: args){
+            if(str == null) return false;
+            try {
+                Float.parseFloat(str);
+            } catch (Exception e) {
+                return false;
+            }
+            if(Float.parseFloat(str) <= 0) return false;
+        }
+        return true;
        //magic happens here
     }
 }
