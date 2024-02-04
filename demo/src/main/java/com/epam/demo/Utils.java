e77a574b7c0900b35918336550b7d2c81392d8d0
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-04 20:31:31 +0300

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,33 +5,16 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
 
-        boolean x = false;
-
-        if(args.contains(null) || args == null) {
-
-            x = false;
-
-        } else {
-
-            int count = 0;
-
-            for(String str: args) {
-
-                if(str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("") || str.equals(null) || str.equals("null")) {
-
-                    count++;
-                }
-
+        for(String str: args){
+            if(str == null) return false;
+            try {
+                Float.parseFloat(str);
+            } catch (Exception e) {
+                return false;
             }
-
-            if(count == 0) {
-
-                x = true;
-            }
-
+            if(Float.parseFloat(str) <= 0) return false;
         }
-
-        return x;
+        return true;
        //magic happens here
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,19 +3,14 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
 
-        boolean x = false;
-
-        if(str == null) {
-
-            x = false;
-
-        } else if (!str.startsWith("-") || !str.equals("0") || !str.startsWith(" ") || !str.equals("") || !str.equals("null") || !str.equals(null))  {
-
-            x = true;
-
+        if (str == null) return false;
+        try {
+            Float.parseFloat(str);
+        } catch (Exception e) {
+            return false;
         }
+        return Float.parseFloat(str) > 0;
 
-        return x;
         //code!
     }
 }
